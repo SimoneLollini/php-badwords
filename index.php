af70c8391831a56e8a2aad51c363542147f2f36c
SimoneLollini <lollinisimone@gmail.com>
2022-11-25 16:32:50 +0100

    Add censored word

--- index.php
+++ index.php
@@ -8,11 +8,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bad World</title>
 </head>
 <body>
     <form action="script.php" method="GET">
-        <textarea type="text" name="pharagraph" cols="30" rows="10"></textarea>
+        <textarea style="width:250px" type="text" name="paragraph" cols="30" rows="10" placeholder="Inserisci qua il testo del tuo paragrafo"></textarea>
+        <br>
+        <input style="width:250px" type="text" name="bad_word" placeholder="Inserisci qua la parola censurare">
+        <br>
         <button type="submit">Invia</button>
     </form>
 </body>
--- script.php
+++ script.php
@@ -1,5 +1,11 @@
 <?php
 $chiave="valore di chiave";
-$pharagraph= $_GET['pharagraph'];
-echo $pharagraph;
+$paragraph= $_GET['paragraph'];
+$bad_word=$_GET['bad_word'];
+// echo $paragraph .'<br>';
+// echo $bad_word .'<br>';
+
+$censored_para=str_replace($bad_word, '***', $paragraph);
+
+echo $censored_para;
 ?>
