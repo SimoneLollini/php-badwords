4e9015168f567c727e57ef164b58b617835b48d9
SimoneLollini <lollinisimone@gmail.com>
2022-11-25 17:12:42 +0100

    Add paragraph length

--- script.php
+++ script.php
@@ -2,10 +2,12 @@
 $chiave="valore di chiave";
 $paragraph= $_GET['paragraph'];
 $bad_word=$_GET['bad_word'];
-// echo $paragraph .'<br>';
+echo $paragraph .'<br>';
 // echo $bad_word .'<br>';
 
 $censored_para=str_replace($bad_word, '***', $paragraph);
+echo 'Questo paragrafo è lungo ' .strlen($paragraph) .' caratteri' .'<br>';
 
 echo $censored_para;
+
 ?>
